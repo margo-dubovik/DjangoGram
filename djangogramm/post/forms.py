c50bdfc515607019f278ecd122d3838c891d195c
margo-dubovik <margodubovik531@gmail.com>
2022-08-23 17:12:58 +0300

    README.md edited

--- djangogramm/post/forms.py
+++ djangogramm/post/forms.py
@@ -5,7 +5,7 @@ from taggit.forms import TagField
 
 class PostForm(forms.ModelForm):
     title = forms.CharField(max_length=128)
-    body = forms.CharField(max_length=250)
+    body = forms.CharField(max_length=500)
     tags = TagField()
 
     class Meta:
