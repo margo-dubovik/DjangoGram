e6d9b308f865f206fd37cd70a1cd7b23230cb285
margo-dubovik <margodubovik531@gmail.com>
2022-08-25 17:55:12 +0300

    navbar rebuilt for bootstrap 5.0

--- djangogramm/post/urls.py
+++ djangogramm/post/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('new/', views.new_post),
+    path('new/', views.new_post, name='new-post'),
     path('all/', views.all_posts, name='all-posts'),
     path('<int:pk>', views.post_details, name='post-details'),
     path('like/<int:pk>', views.like_view, name='like_post'),
