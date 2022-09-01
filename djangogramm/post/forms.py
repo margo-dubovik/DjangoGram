fc24ca13a6ac3a31f16241eb5cbc4577578f30b7
margo-dubovik <margodubovik531@gmail.com>
2022-09-01 16:37:45 +0300

    form for new post modified

--- djangogramm/post/forms.py
+++ djangogramm/post/forms.py
@@ -5,7 +5,7 @@ from taggit.forms import TagField
 
 class PostForm(forms.ModelForm):
     title = forms.CharField(max_length=128)
-    body = forms.CharField(max_length=500)
+    body = forms.CharField(widget=forms.Textarea(attrs={'class': 'form-control', 'rows': "5"}))
     tags = TagField(label="Tags (comma-separated, no spaces):")
 
     class Meta:
